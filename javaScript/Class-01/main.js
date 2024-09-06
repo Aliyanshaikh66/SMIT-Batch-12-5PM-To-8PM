952d1e1d047ce7695b926b8cfb6c832b27d08139
aliyanshaikh66 <aliyanzahid02@gmail.com>
2024-09-06 19:41:24 +0500

    add

---
+++ javaScript/Class-01/main.js
@@ -0,0 +1,12 @@
+// alert("Welcome To JavaScript");
+// console.log(alert);
+
+
+var x = '5';
+var y = '5';
+var z 
+console.log(z);
+
+
+// var value double codes value merge donot plus 
+// var value without codes value merge addition
