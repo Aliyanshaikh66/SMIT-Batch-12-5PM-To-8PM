82b0cacd0dae0a781c7502f350aaef66ba91bcd6
aliyanshaikh66 <aliyanzahid02@gmail.com>
2024-09-13 19:51:01 +0500

    last complete math familler Operators

--- javaScript/Class-01/main.js
+++ javaScript/Class-01/main.js
@@ -2,11 +2,46 @@
 // console.log(alert);
 
 
-var x = '5';
-var y = '5';
-var z 
-console.log(z);
+// var x = '5';
+// var y = '5';
+// var z 
+// console.log(z);
 
 
 // var value double codes value merge donot plus 
 // var value without codes value merge addition
+
+
+// var name1 = '5';
+// var city = '5';
+// var merge = name1+ city;
+// console.log (merge)
+
+
+let x = 15;
+x + 15;
+console.log(x + 15);
+
+
+
+// var originalnum = 27;
+// var newnum = originalnum + 1;
+// console.log(newnum);
+
+
+
+var originalNum = 27;
+var newnum = originalNum + 1;
+console.log(newnum);
+
+
+
+
+
+
+
+
+
+
+
+
